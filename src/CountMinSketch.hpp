a3e75602ec9af6cd80e0237c83df4d6e8992a1ce
Mun Hou <mhwong2007@gmail.com>
2021-02-23 15:40:22 +0800

    include string header

--- src/CountMinSketch.hpp
+++ src/CountMinSketch.hpp
@@ -17,6 +17,7 @@
 #pragma once
 
 #include <algorithm>
+#include <string>
 
 namespace MIDAS {
 struct CountMinSketch {
--- src/NormalCore.hpp
+++ src/NormalCore.hpp
@@ -17,6 +17,7 @@
 #pragma once
 
 #include <cmath>
+#include <string>
 #include "CountMinSketch.hpp"
 
 namespace MIDAS {
